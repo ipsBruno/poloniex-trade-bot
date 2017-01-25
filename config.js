1df08ec450ad685449c2a17f3562f855a3815f64
Bruno da Silva <email@brunodasilva.com>
2017-01-25 21:37:09 -0200

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,21 @@
 var config = {};
 
-// Market option
 config.watch = {
   currency: 'USDT',
   asset: 'BTC'
 }
 
-//Trader option
+config.tradingAdvisor = {
+  candleSize: 300,  //(candlestick period in seconds; valid values are 300, 900, 1800, 7200, 14400, and 86400),
+  historySize:45
+}
+
+
+
 config.trader = {
-  enabled: false,
-  key: '',
-  secret: '',
+  enabled: true,
+  key: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+  secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
 }
 
 module.exports = config;
