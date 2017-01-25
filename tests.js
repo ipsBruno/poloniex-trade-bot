982ce0a3e4699a0abd2220eb05b77497149f0b06
Bruno da Silva <email@brunodasilva.com>
2017-01-25 19:06:24 -0200

    Update tests.js

--- tests.js
+++ tests.js
@@ -3,8 +3,7 @@
 
 // Importar os módulos
 var bot = require('./core.js')
-var config = require('./config.js');
-var trader = config.trader;
+var trader = bot.config.trader;
 
 var init = function(){
 	bot.balance(function(err, currency, asset){
@@ -13,8 +12,8 @@ var init = function(){
 	 		return console.log(err)
 	 	}
 		
-		console.log("Saldo em %s: %s ", config.watch.currency ,currency);
-		console.log("Saldo em %s: %s ", config.watch.asset ,asset);
+		console.log("Saldo em %s: %s ", bot.config.watch.currency ,currency);
+		console.log("Saldo em %s: %s ", bot.config.watch.asset ,asset);
 
 		
 		bot.lastPrice(function(pair, data){
@@ -37,10 +36,8 @@ var isUpTrend = function(pair, last){
 }
 
 if (trader.enabled == true){
-	bot.setCredential(config.trader.key, config.trader.secret);
-	bot.setPair(config.watch.currency + '_' + config.watch.asset);
-	
-	//inicia bot
+	bot.setCredential(bot.config.trader.key, bot.config.trader.secret);
+	bot.setPair(bot.config.watch.currency + '_' + bot.config.watch.asset);
 	init();
 } else {
 	console.log("Trader inativo > config.js > trader.enable: false");
@@ -48,51 +45,3 @@ if (trader.enabled == true){
 
 
 
-
-/*
- // Configurar o BOT
- bot.setCredential(config.trader.key, config.trader.secret);
- bot.setPair(config.watch.currency + '_' + config.watch.asset);
- 
- var minTradeUsd = 10;
- 
- bot.balance(function(err, usd, btc) {
- 	console.log('aaaaaaaa')
- 	if (err) {
- 		return console.log(err)
- 	}
- 	if (usd < minTradeUsd) {
- 		return console.log("Você precisa ter " + minTradeUsd + ". Mas você tem " + usd + " USDT")
- 	}
- 	
- 	var cotSell = 924
- 	var cotBuy = 916
- 	var amount = usd / cotBuy
- 	
- 	bot.buycoin(cotBuy, amount, function(err, data) {
- 		if (err) {
- 			return console.log(err)
- 		}
- 		var orderid = data.orderNumber;
- 		console.log("ORDER: BUY | RATE: %f | ID: %f | Amount: %f", cotBuy, orderid, amount)
- 		setInterval(function() {
- 			if (!bot.isOpen(orderid)) {
- 				bot.sellcoin(cotSell, usd * cotBuy, function(err, data) {
- 					if (err) {
- 						return console.log(err)
- 					}
- 					console.log("ORDER: SELL | RATE: %f | ID: %f", cotSell, orderid)
- 					var orderid = data.orderNumber;
- 					setInterval(function() {
- 						if (!bot.isOpen(orderid)) {
- 							console.log("TRADE EFETUADO")
- 						}
- 					}, 1000)
- 				})
- 			}
- 		}, 10000)
- 	})
- })
- 
- 
- */
