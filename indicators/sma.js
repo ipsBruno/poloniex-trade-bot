949128bfb8c5a4b7be7f4e2de22d59ca6596ce02
Bruno da Silva <email@brunodasilva.com>
2017-01-25 19:58:58 -0200

    Update sma.js
    max min config

--- indicators/sma.js
+++ indicators/sma.js
@@ -1,61 +1,58 @@
-
 /*
-* Carregar SMA da biblioteca technicalindicators
-*/
-
+ * Carregar SMA da biblioteca technicalindicators
+ */
 const technicalindicators = require('technicalindicators').SMA;
-
-
 /*
-* Configurar os valores indicadores 
-* Para o método SMA
-* @short/long: periodos
-* @thresholds: valores para considerar buy/sell. 
-* Qualquer valor acima de 0.9 indica uma alta. Valores muito acima de 1.5 indicam uma queda de correção
-*/
-
+ * Configurar os valores indicadores 
+ * Para o método SMA
+ * @short/long: periodos
+ * @thresholds: valores para considerar buy/sell. 
+ * Qualquer valor acima de 0.9 indica uma alta. Valores muito acima de 1.5 indicam uma queda de correção
+ */
 const config = {
-  short: 10,
-  long: 21,
-  thresholds: 0.9
+	short: 10,
+	long: 21,
+	thresholds: {
+		min: 0.9,
+		max: 1.5
+	}
 };
-
 /*
  * Essa função serve pra calcular o MACD
  * @prices: valores de entrada
  */
-
 exports.calculate = function(prices) {
-
 	// Criar dois vetores para armazenar os períodos short/long
 	var line1 = []
 	var line2 = []
-
 	// calcular os períodos
-	var sma = new technicalindicators({period : config.short, values : prices}) 
+	var sma = new technicalindicators({
+		period: config.short,
+		values: prices
+	})
 	prices.forEach(price => {
 		var result = sma.nextValue(price);
-		if(result) {
+		if (result) {
 			// adicionar short ao line1
 			line1.push(result)
 		}
 	});
-
 	// calcular os períodos
-	var sma = new technicalindicators({period : config.long, values : prices}) 
+	var sma = new technicalindicators({
+		period: config.long,
+		values: prices
+	})
 	prices.forEach(price => {
 		var result = sma.nextValue(price);
-		if(result) {
+		if (result) {
 			// adicionar long ao line2
 			line2.push(result)
 		}
 	});
-
 	// pegar últimos valores short/long
-	var lastshort = line1[line1.length-1]
-	var lastlong = line2[line2.length-1]
-
+	var lastshort = line1[line1.length - 1]
+	var lastlong = line2[line2.length - 1]
 	// caso a tendencia de short é maior que a de long
 	// caso for true = tendencia de alta / caso for false tendencia de baixa
-	return lastshort / lastlong > config.thresholds;
+	return config.thresholds.max > (lastshort / lastlong) > config.thresholds.min;
 }
