8b303952e9bcc33202b0753831b6fa6505a6c26f
Bruno da Silva <email@brunodasilva.com>
2017-01-26 22:18:45 -0200

    Update config.js
    value fix

--- config.js
+++ config.js
@@ -1,22 +1,17 @@
 var config = {};
- 
 config.watch = {
-  exchange: 'poloniex',
-  currency: 'USDT',
-  asset: 'BTC'
+	exchange: 'poloniex',
+	currency: 'USDT',
+	asset: 'BTC'
 }
-
 config.tradingAdvisor = {
-  candleSize: 300,  //(candlestick period in seconds; valid values are 300, 900, 1800, 7200, 14400, and 86400),
-  historySize:45
+	candleSize: 300, //(candlestick period in seconds; valid values are 300, 900, 1800, 7200, 14400, and 86400),
+	historySize: 45
 }
-
-
-
 config.trader = {
-		  enabled: false,
-		  key: 'xxxxxxxxxx',
-		  secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxx',
-		}
-
+	value: 10.0,
+	enabled: false,
+	key: 'xxxxxxxxxx',
+	secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxx',
+}
 module.exports = config;
