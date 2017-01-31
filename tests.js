d201391e3d609377c72e223e8b58ad0da2277b2b
Rafael Cabrera <meirarc@gmail.com>
2017-01-31 21:00:01 -0200

    atualização de logs e profit analysis (em teste)

--- tests.js
+++ tests.js
@@ -1,140 +1,117 @@
 // Importar os módulos
 var config = require('./config.js');
+var log = require('./core/log.js');
+var profit = require('./core/profit.js');
 var BOT = require('./exchanges/' + config.watch.exchange + '.js');
 
-var SMA = require('./indicators/sma.js')
-var MACD = require('./indicators/macd.js')
-var RSI = require('./indicators/rsi.js')
 var EMASMA = require('./indicators/emasma.js')
 
-
-
-//Limpar tela de console
-process.stdout.write('\x1Bc');
-
-
-//Adicionar titulo ao projeto
-process.title = '[POLONIEX BOT]'
-
+//Comandos de tela
+process.stdout.write('\x1Bc'); 		//Limpar tela de console
+process.title = '[POLONIEX BOT]';	//Adicionar titulo ao projeto
 
 // Mandar mensagem indicando inicialização do BOT
-console.log("\033[35m[Info] O BOT foi iniciado. Estou analisando mercado agora...")
+log.info('start', "O BOT foi iniciado. Estou analisando mercado agora...");
 
-var buyOrderId = -1
-var sellOrderId = -1
+var buyOrderId = -1;
+var sellOrderId = -1;
 
-var sellPrice 
-var buyPrice 
-
-var debug = false
-
-///Testando função para utilizar indicadores em conjunto
-//TODO: Contruir lógica para utilização dos indicadores ativos
-//TODO: Incluir uma configuração de enable no config para cada indicador
-function calculate(candles){
-	var upSMA, upMACD, upRSI, upEMASMA;
-	
-	console.log(upSMA, upMACD, upRSI, upEMASMA);
-	
-	if (config.SMA.enable = true) upSMA = SMA.calculate(candles);
-	if (config.MACD.enable = true) upMACD = MACD.calculate(candles);
-	if (config.RSI.enable = true) upRSI = RSI.calculate(candles);
-	if (config.EMASMA.enable = true) upEMASMA = EMASMA.calculate(candles);
-	
-	console.log(upSMA, upMACD, upRSI, upEMASMA);
-}
-
+var sellPrice;
+var buyPrice;
 
 function manipularOrdens() {
-		// Selecionar os close dos candlesticks
-		var candles = BOT.getCandles()
+	log.info('debug', "manipularOrdens >> started");
+	
+	// Selecionar os close dos candlesticks
+	var candles = BOT.getCandles();
+	
+	//Se EMASMA: true
+	var buyable = EMASMA.calculate(candles);
+	log.info('debug', "manipularOrdens >> buyable: "+buyable);
 
-		// Enviar os closes para calcular o SMA
-		//TODO: Montar uma lógica para ativar e desativar os indicadores pelo config.js
-		//var buyable = SMA.calculate(candles)
-		//var buyable = MACD.calculate(candles)
-		//var buyable = RSI.calculate(candles)
-		var buyable = EMASMA.calculate(candles)
+	
+	if ( buyOrderId == -1 && sellOrderId == -1) { 
+		log.info('debug', "manipularOrdens >> Não existe nenhuma ordem aberta");
+
+		if (buyable == true) {
+
+			// remover 0.1% p/ comprar
+			buyPrice = parseFloat(BOT.rempercent(parseFloat(BOT.pairTicker.highbid), 0.1));
+			buyPrice = buyPrice.toFixed(3);
+			log.info('debug', "manipularOrdens >> buyPrice: "+ buyPrice);
+
+			// adicionar 0.4% pra vender
+			sellPrice = parseFloat(BOT.addpercent(parseFloat(BOT.pairTicker.lowask), 0.2));
+			sellPrice = sellPrice.toFixed(3);
+			log.info('debug', "manipularOrdens >> sellPrice: "+ sellPrice);
+
+			buyOrderId = 0
+			log.info('debug', "manipularOrdens >> buyOrderId: "+ buyOrderId);
+
+			// ajustar as fee
+			bitcoinsVal =  parseFloat(BOT.rempercent(parseFloat(BOT.config.trader.value/buyPrice), 0.25));
+			bitcoinsVal = bitcoinsVal.toFixed(8);
+			log.info('debug', "manipularOrdens >> bitcoinsVal: "+ bitcoinsVal);
+
+			BOT.buycoin(buyPrice, BOT.config.trader.value/buyPrice, function(err, data) {
+
+					if(err){
+						buyOrderId = -1
+						log.info('error', "manipularOrdens >> " + err);
+						return false
+					}
+					
+
+					buyOrderId = data.orderNumber
+					
+					log.info('debug', "manipularOrdens >> buyOrderId: " + buyOrderId + " price: " + buyPrice + " bitcoinsVal: " + bitcoinsVal);
+					log.info('trade', "manipularOrdens >> Preço: "+buyPrice+" | Qtd: " + bitcoinsVal);
+			})				
+		}
+	}
+
+
+	if( buyOrderId > 0 && sellOrderId == -1) {
+		log.info('debug', "manipularOrdens >> A ordem de compra está aberta e a ordem de venda fechada");
 		
-		//TODO:Validar função
-		//calculate(candles);
+		if ( !BOT.isOpen(buyOrderId)) {
+			log.info('debug', "manipularOrdens >> A ordem de compra já foi finalizda");
+			
+
+			sellOrderId = 0
+			buyOrderId = 0
+			log.info('debug', "manipularOrdens >> sellOrderId: " + sellOrderId + " buyOrderId: "+ buyOrderId);
+			
+			log.info('debug', "manipularOrdens >> criar uma ordem de venda com saldo em btc");
+			BOT.sellcoin(parseFloat(BOT.addpercent(parseFloat(sellPrice), 0.25)).toFixed(2), bitcoinsVal, function(err, data) {
+
+					if(err){
+						sellOrderId = -1
+						buyOrderId = 1
+						log.info('error', "manipularOrdens >> " + err);
+						return false
+							
+					}
+					
+					sellOrderId = data.orderNumber
+
+					log.info('debug', "manipularOrdens >> sellOrderId: " + sellOrderId + " price: " + sellPrice + " bitcoinsVal: " + bitcoinsVal);
+					log.info('trade', "manipularOrdens >> Preço: "+sellPrice+" | Qtd: "+ bitcoinsVal);
+			})	
+		}
+	}
+	
+	if( sellOrderId > 0 && buyOrderId == 0) {
+		log.info('debug', "manipularOrdens >> Ambas ordens já foram abertas");
 		
-		//var buyable = true
-
-		// Caso não estiver nenhuma ordem aberta
-		if ( buyOrderId == -1 && sellOrderId == -1) { 
-
-			if (buyable == true) {
-
-				// remover 0.1% p/ comprar
-				buyPrice = parseFloat(BOT.rempercent(parseFloat(BOT.pairTicker.highbid), 0.1))
-				buyPrice = buyPrice.toFixed(3)
-
-				// adicionar 0.4% pra vender
-				sellPrice = parseFloat(BOT.addpercent(parseFloat(BOT.pairTicker.lowask), 0.2))
-				sellPrice = sellPrice.toFixed(3)
-
-				buyOrderId = 0
-
-				// ajustar as fee
-				bitcoinsVal =  parseFloat(BOT.rempercent(parseFloat(BOT.config.trader.value/buyPrice), 0.25))
-				bitcoinsVal = bitcoinsVal.toFixed(8)
-
-				BOT.buycoin(buyPrice, BOT.config.trader.value/buyPrice, function(err, data) {
-
-						if(err){
-							buyOrderId = -1
-							if(debug) console.log("\033[31mBuyError", err)
-							return false
-						}
-						
-
-						buyOrderId = data.orderNumber
-
-						console.log("\033[32mComprando\033[37m | Preço: " + buyPrice );
-				})				
-			}
+		if ( !BOT.isOpen(sellOrderId)) {
+			log.info('debug', "manipularOrdens >> Orderm de venda fechada");
+			
+			log.info('info', "manipularOrdens >> Trade efetuado com sucesso");
+			sellOrderId = -1
+			buyOrderId = -1
 		}
-
-
-		// Caso a ordem de compra estiver aberta e a ordem de venda fechada
-		if( buyOrderId > 0 && sellOrderId == -1) {
-
-			// caso a ordem de compra já foi finalizda
-			if ( !BOT.isOpen(buyOrderId)) {
-				
-
-				sellOrderId = 0
-				buyOrderId = 0
-
-				// criar uma ordem de venda com saldo em btc
-				BOT.sellcoin(parseFloat(BOT.addpercent(parseFloat(sellPrice), 0.25)).toFixed(2), bitcoinsVal, function(err, data) {
-
-						if(err){
-							sellOrderId = -1
-							buyOrderId = 1
-							if(debug) console.log("\033[31mSellError", err)
-							return false
-								
-						}
-						
-						sellOrderId = data.orderNumber
-
-						console.log("\033[31mVendendo\033[37m | Preço: " + sellPrice );
-				})	
-			}
-		}
-
-		// caso ambas ordens já foram abertas
-		if( sellOrderId > 0 && buyOrderId == 0) {
-			// verificar se há ordem de venda fechada
-			if ( !BOT.isOpen(sellOrderId)) {
-					// caso ordem de venda estiver fechada fazer o trade
-					console.log("\033[34mTrade efetuado com sucesso\033[37m");
-					sellOrderId = -1
-					buyOrderId = -1
-			}
-		}
+	}
 }
 
 
@@ -143,15 +120,25 @@ function checkAccount(){
 	
 	var currency = BOT.balance(BOT.config.watch.currency)
 	var asset = BOT.balance(BOT.config.watch.asset)
+	
+
 
 	if( currency <  BOT.config.trader.value) {
-		return console.log("\033[31m[Erro] Você não tem o mínimo  para Trades | MIN %s: %s",BOT.config.watch.currency, BOT.config.trader.value)
+		return log.info('debug', "checkAccount >> Você não tem o mínimo  para Trades | MIN "+BOT.config.watch.currency+": " +BOT.config.trader.value );
 	}
 
-
-	console.log("\033[36m[Info] Saldo em %s: %s ", BOT.config.watch.currency ,currency);
-	console.log("\033[36m[Info] Saldo em %s: %s ", BOT.config.watch.asset ,asset);
-
+	log.info('info', "Saldo em "+BOT.config.watch.currency+": "+currency);
+	log.info('info', "Saldo em "+BOT.config.watch.asset+":  " + asset);
+	
+	profit.init(currency, asset);
+	
+	//Imprime de 30 em 30 minutos o saldo
+	setInterval(function(){
+		log.info('info', "Saldo em " + BOT.config.watch.currency + ": " + currency);
+		log.info('info', "Saldo em " + BOT.config.watch.asset + ":  " + asset);
+		profit.info(currency, asset);
+	},1800000);
+	
 	// caso ele tenha saldo começar a manipular as ordens
 	setInterval( function c(){ manipularOrdens(); return c; }()  , 15000 )
 }
@@ -166,14 +153,14 @@ if (BOT.config.trader.enabled){
 	var check = setInterval(
 		function() {
 			if(!BOT.lastPrice() > 0) {
-				console.log("\033[31mRecebendo informações de preço ..."  );
+				log.info('warn', "Recebendo informações de preço ...");
 			}
 			else if( !(BOT.getCandles().length > 0)) {
-				console.log("\033[31mRecebendo informações de candlesticks ..." );
+				log.info('warn', "Recebendo informações de candlesticks ...");
 			}
 			
 			else if(!(BOT.balanceVal[BOT.config.watch.currency] > 0)) {
-				console.log("\033[31mRecebendo informações da sua conta ..." );
+				log.info('warn', "Recebendo informações da sua conta ...");
 			}
 			else {
 				checkAccount()
@@ -181,6 +168,6 @@ if (BOT.config.trader.enabled){
 			}	
 		}, 10000)	
 } else {
-	console.log("\033[31m[Erro] Trader inativo. Edite config.js para trader.enable: true");
+	log.info('error', "Trader inativo. Edite config.js para trader.enable: true");
 }
 
